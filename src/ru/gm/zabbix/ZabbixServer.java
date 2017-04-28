fd99d47c8aa5d8fa83969f05d1503ceac22e2f86
neiromc <neiromc@gmail.com>
2017-04-28 17:22:21 +0300

    Try to fix zabbix answer in ZabbixObject.send(). Recompile jar library

--- src/ru/gm/zabbix/ZabbixServer.java
+++ src/ru/gm/zabbix/ZabbixServer.java
@@ -63,7 +63,7 @@ public class ZabbixServer {
             }
 
             if ( rCount < 13) {
-                return "empty_answer_array";
+                return "empty_answer";
             } else {
                 return new String(responseData, 13, rCount - 13, StandardCharsets.UTF_8);
             }
